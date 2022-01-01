cc54063f080791b901ebdb25446f4b844ecde122
Benjamin Bray <benrbray@gmail.com>
2022-01-02 00:27:09 +0900

    rename Server -> SmartServer ; convert SmartServer from object to class and expose intermediate values for testing

--- src/main/scala/prices/Main.scala
+++ src/main/scala/prices/Main.scala
@@ -5,6 +5,10 @@ import cats.effect.{ IO, IOApp }
 import prices.config.Config
 
 object Main extends IOApp.Simple {
-  def run: IO[Unit] = Config.load[IO].flatMap(Server.serve(_).compile.drain)
+
+  def run: IO[Unit] = Config.load[IO].flatMap(config => {
+    val server = new SmartServer(config)
+    server.serve().compile.drain
+  })
 
 }
