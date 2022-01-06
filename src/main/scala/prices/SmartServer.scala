764bbdcb046f34347f790d0473bf46182c28eedb
Benjamin Bray <benrbray@gmail.com>
2022-01-06 17:39:11 +0900

    move route definitions into SmartApp object, for easier testing

--- src/main/scala/prices/SmartServer.scala
+++ src/main/scala/prices/SmartServer.scala
@@ -1,19 +1,39 @@
 package prices
 
 import cats.effect._
+import cats.syntax.semigroupk._
+import cats.data.Kleisli
+
 import com.comcast.ip4s._
 import fs2.Stream
+
+import org.http4s._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.server.middleware.Logger
-import cats.syntax.semigroupk._
 
 import prices.config.Config
 import prices.routes.InstanceKindRoutes
 import prices.routes.PriceRoutes
 import prices.services.SmartcloudClientService
+import prices.services.ClientService
 
 ////////////////////////////////////////////////////////////
+
+object SmartApp {
+
+  type Routes[F[_]] = Kleisli[F, Request[F], Response[F]]
+
+  def make[F[_]: Sync](
+    clientService: ClientService[F]
+  ): Routes[F]
+    = (
+      InstanceKindRoutes[F](clientService).routes
+      <+> PriceRoutes[F](clientService).routes
+    ).orNotFound
+
+}
+
 class SmartServer(config: Config) {
 
   val httpClientRes = EmberClientBuilder.default[IO].build
@@ -26,10 +46,7 @@ class SmartServer(config: Config) {
       httpClientRes
     )
 
-  val httpApp = (
-    InstanceKindRoutes[IO](clientService).routes
-    <+> PriceRoutes[IO](clientService).routes
-  ).orNotFound
+  val httpApp = SmartApp.make(clientService)
 
   val serverRes = EmberServerBuilder
     .default[IO]
